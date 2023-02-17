b69735b07e1b038c55188897e5afed30b7e63b18
Eshita <79079168+eshitavyas@users.noreply.github.com>
2023-02-17 22:55:16 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "SRN-PES2UG20CS575,NAME-ESHITA VYAS\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  #cout << "I have successfully built and run\n";
   return 0;
 }
